72cb4015ddc7108f0305a58ef5a2304b35c99dab
Icedream <icedream2k9@die-optimisten.net>
2011-08-09 19:45:33 +0200

    Fixed the latest push. Obviously comes from that MathTest project I used to test my MathParser.

---
+++ CSharpBot/MathParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MSScriptControl;
+
+namespace CSharpBot
+{
+    public class MathParser
+    {
+        ScriptControl _sctl;
+
+        public MathParser()
+        {
+            _sctl = new ScriptControl();
+            //_sctl.AllowUI = false;
+            _sctl.Language = "VBScript";
+        }
+
+        public void AddVariable(string name, dynamic value)
+        {
+            _sctl.AddObject(name, value);
+        }
+
+        public void Reset()
+        {
+            _sctl.Reset();
+        }
+
+        public string Calculate(string expression)
+        {
+            object result = _sctl.Eval(expression);
+            //if (result == null)
+            //    throw new Exception("Could not calculate that expression.");
+            return result.ToString();
+        }
+    }
+}
