9b02bc07c89f074f16ee33fc064bc85b1b687458
Justin D <jdphenix@gmail.com>
2011-08-09 00:18:24 -0500

    Refactored my extension methods to Util.cs

--- CSharpBot/Math.cs
+++ CSharpBot/Math.cs
@@ -7,33 +7,6 @@ namespace CSharpBot
 {
     namespace Math
     {
-        public static class StringHelper
-        {
-            static public bool EqualsAny(this string sh, string[] sts)
-            {
-                foreach (string s in sts)
-                {
-                    if (sh.Equals(s))
-                        return true; 
-                }
-                return false;
-            }
-        }
-
-        public static class CharHelper
-        {
-            /// <summary>
-            /// True if this char is equal to any characters in the specified
-            /// character array. 
-            /// </summary>
-            /// <param name="ch">char to compare. </param>
-            /// <param name="ca">char array to compare against. </param>
-            static public bool EqualsAny(this char ch, char[] ca)
-            {
-                return ca.Any(ch.Equals);
-            }
-        }
-
         /// <summary>
         /// Provides basic math expression parsing functionality. 
         /// </summary>
