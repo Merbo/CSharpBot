2365083355d1a3ef5935cb198b8311a4156a1925
Merbo <carrethan02@gmail.com>
2011-08-11 12:05:02 -0400

    Massbuilt, you can test your applications in the compiler GUI now.

--- CSharpBot Script Compiler/GUI.Designer.cs
+++ CSharpBot Script Compiler/GUI.Designer.cs
@@ -34,6 +34,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -53,7 +54,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(107, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Select File";
             this.button1.UseVisualStyleBackColor = true;
@@ -81,11 +82,21 @@
             // 
             this.button2.Location = new System.Drawing.Point(15, 81);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(104, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Compile";
+            this.button2.Text = "Compile File";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Test File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // GUI
             // 
@@ -93,6 +104,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(426, 217);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -114,5 +126,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- CSharpBot Script Compiler/GUI.cs
+++ CSharpBot Script Compiler/GUI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
 
 namespace GUI
 {
@@ -27,13 +28,17 @@ namespace GUI
             textBox1.Text = openFileDialog1.FileName;
             if (openFileDialog1.FileName.EndsWith(".csb"))
             {
-                label2.ForeColor = Color.Green;
+                label2.ForeColor = Color.Lime;
                 label2.Text = "Compilable!";
+                button2.Enabled = true;
+                button3.Enabled = false;
             }
             else
             {
                 label2.ForeColor = Color.Red;
                 label2.Text = "Not Compilable.";
+                button2.Enabled = false;
+                button3.Enabled = false;
             }
         }
 
@@ -41,10 +46,14 @@ namespace GUI
         {
             label2.ForeColor = Color.Yellow;
             label2.Text = "Not Selected.";
+            button2.Enabled = false;
+            button3.Enabled = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            label2.ForeColor = Color.Cyan;
+            label2.Text = "Being Compiled.";
             try
             {
                 Scanner scanner = null;
@@ -54,12 +63,41 @@ namespace GUI
                 }
                 Parser parser = new Parser(scanner.Tokens);
                 CodeGen codeGen = new CodeGen(parser.Result, Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + ".exe");
+                label2.ForeColor = Color.Lime;
+                label2.Text = "Compiled! Check the directory this program is in, or click 'Test File' and check the console.";
+                button3.Enabled = true;
             }
             catch (Exception ex)
             {
                 Console.Error.WriteLine(ex.Message);
+                label2.ForeColor = Color.Red;
+                label2.Text = "Error-prone. Check the console.";
+                button3.Enabled = false;
             }
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            string[] fn = openFileDialog1.FileName.Split('\\');
+            try
+            {
+                start.FileName = fn.Last().Replace(".csb", ".exe");
+                start.UseShellExecute = false;
+                start.RedirectStandardOutput = true;
+                using (Process process = Process.Start(start))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string result = reader.ReadToEnd();
+                        Console.Write(result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
 }
