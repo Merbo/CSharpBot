d9a8442ae69f13af3c5febdfcfc4f7a61aa86f3f
Merbo <carrethan02@gmail.com>
2011-08-11 11:25:07 -0400

    Changed colors of GUI, it works with compilation now :)

--- CSharpBot Script Compiler/GUI.Designer.cs
+++ CSharpBot Script Compiler/GUI.Designer.cs
@@ -62,6 +62,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 13);
@@ -90,6 +91,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(426, 217);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
@@ -97,7 +99,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "GUI";
-            this.Text = "GUI";
+            this.Text = "CSharpBot Script Compiler GUI";
             this.Load += new System.EventHandler(this.GUI_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
