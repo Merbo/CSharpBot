825647703286e838bef72bc0716293c00b9197fd
Merbo <carrethan02@gmail.com>
2011-08-15 18:52:02 -0400

    Added this cool comment thingy (tm) into the Log() function code in Client.cs of SimpleLiveClient :)

--- SimpleLiveClient/Client.cs
+++ SimpleLiveClient/Client.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Net.Sockets;
 using System.Threading;
+using System.Linq;
 using Client;
 
 namespace Client
@@ -17,33 +18,78 @@ namespace Client
         public static TcpClient socket;
         public static StreamReader reader;
         public static StreamWriter writer;
-        //There are 'shortcuts' for our functions :)
+
+        //Tells us whether we are debugging - only enabled via arguement --debug
+        public static bool Debug;
+
+        //Creating aliases for our functions :)
         static string read()
         {
             return Functions.read();
         }
-        static string read(StreamReader reader)
+        static string read(StreamReader r)
         {
-            return Functions.read(reader);
+            return Functions.read(r);
         }
         static void write(string data)
         {
             Functions.write(data);
         }
-        static void write(string data, StreamWriter writer)
+        static void write(string data, StreamWriter r)
         {
-            Functions.write(data, writer);
+            Functions.write(data, r);
+        }
+
+        static void Log(string data, int level = 6)
+        {
+            /*
+             * <> Means necessary parameter
+             * [] Means optional parameter
+             * 
+             * Syntax: Log(<string>, [int])
+             * 
+             * Action: Writes <string> to the console, also can use presets based off of simple tasks, via use of [int]
+             * 
+             * By default, this will simply do a white console.WriteLine of <string>.
+             * However, [int] can be:
+             * 0: Error
+             * 1: Warning
+             * 2: Notice
+             * 3: Debug*
+             * 4: Log*
+             * 5: Setup**
+             * 6: Plain old Console.WriteLine
+             * (*: Only runs if 'Debug' evaluates to true)
+             * (**: Only meant for use at the beginning setup configuration. 
+            */
+            Functions.Log(data, level);
         }
 
         static void Main(string[] args)
         {
+            //process args.
+            foreach (string arg in args)
+            {
+                string[] parameters = arg.Split('=');
+                string name = parameters[0].ToLower();
+                string value = string.Join("=", parameters.Skip(1).ToArray());
+                switch (name)
+                {
+                    case "-d":
+                    case "--debug":
+                        Debug = true;
+                        continue;
+                }
+            }
+
+
             tryserver:
-            Console.Write("Server: ");
+            Log("Server: ", 5);
             server = Console.ReadLine();
             if (server == "")
                 goto tryserver;
             tryport:
-            Console.Write("Port: ");
+            Log("Port: ", 5);
             string tmp = Console.ReadLine();
             if (int.TryParse(tmp, out port))
             {
@@ -52,25 +98,22 @@ namespace Client
                 }
                 else
                 {
-                    Console.WriteLine("Invalid port.");
+                    Log("Invalid port.", 0);
                     goto tryport;
                 }
             }
             else
             {
-                Console.WriteLine("Invalid Port.");
+                Log("Invalid Port.", 0);
                 goto tryport;
             }
             trynick:
-            Console.Write("Nick: ");
+            Log("Nick: ", 5);
             nick = Console.ReadLine();
             if (nick == "")
                 goto trynick;
-            trypass:
-            Console.Write("Pass (if given): ");
+            Log("Pass (if given): ", 5);
             pass = Console.ReadLine();
-            if (pass == "")
-                goto trypass;
             try
             {
                 socket = new TcpClient(server, port);
@@ -79,9 +122,13 @@ namespace Client
                 NetworkStream stream = socket.GetStream();
                 reader = new StreamReader(stream);
                 writer = new StreamWriter(stream);
+                Log("Logging in...");
                 write("NICK " + nick);
                 if (pass != "")
+                {
+                    Log("Sending password...");
                     write("PASS " + pass);
+                }
                 string input = Console.ReadLine().ToLower();
                 while (!input.Equals("/quit"))
                 {
@@ -90,15 +137,28 @@ namespace Client
                     {
                         string[] split = data.Split(' ');
                         string[] cmd = split[0].Split('/');
+                        string aftercommand = string.Join(" ", split.Skip(1));
                         switch (cmd[1])
                         {
                             case "say":
-                                write("MSG " + input);
+                                write("PRIVMSG " + input);
+                                break;
+                            case "nick":
+                                write("NICK " + aftercommand);
+                                break;
+                            case "kill":
+                                write("KILL " + aftercommand);
+                                break;
+                            case "quit":
+                                write("QUIT " + aftercommand);
+                                break;
+                            default:
+                                Log("You cant just create your own commands!", 1);
                                 break;
                         }
                     }
                     else
-                        write("MSG " + input);
+                        write(input);
                 }
                 reader.Close();
                 writer.Close();
@@ -106,10 +166,10 @@ namespace Client
             }
             catch
             {
-                Console.WriteLine("Failed to connect :(");
+                Log("Failed to connect", 0);
             }
+            Log("Terminating...", 3);
+            Console.ReadKey();
         }
-
-
     }
 }
--- SimpleLiveClient/Functions.cs
+++ SimpleLiveClient/Functions.cs
@@ -9,15 +9,71 @@ namespace Client
 {
     public class Functions
     {
+        public static bool Debug = Client.Debug;
+        public static void Log(string data, int level = 6)
+        {
+            switch (level)
+            {
+                //our priority is based off of the height of our number.
+                //For example, 0 is a huge exception, but 3 might be a debug message.
+                //However, 5 is for setup ONLY, and 6 is an alias for Console.WriteLine.
+                case 0:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR: " + data);
+                    Console.ResetColor();
+                    break;
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("WARNING: " + data);
+                    Console.ResetColor();
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("NOTICE: " + data);
+                    Console.ResetColor();
+                    break;
+                case 3:
+                    if (Debug)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("DEBUG: " + data);
+                        Console.ResetColor();
+                    }
+                    break;
+                case 4:
+                    if (Debug)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("LOG: " + data);
+                        Console.ResetColor();
+                    }
+                    break;
+                //exception from leveling: Setup.
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Write("SETUP: " + data);
+                    Console.ResetColor();
+                    break;
+                //exception from leveling: Simple Console.WriteLine.
+                case 6:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(data);
+                    Console.ResetColor();
+                    break;
+            }
+        }
         public static string read(StreamReader reader)
         {
             try
             {
-                return reader.ReadLine();
+                string line = reader.ReadLine();
+                Log(line);
+                return line;
             }
             catch
             {
-                Console.WriteLine("Unable to read from server");
+               Log("Unable to read from server", 0);
                 return null;
             }
         }
@@ -25,11 +81,13 @@ namespace Client
         {
             try
             {
-                return Client.reader.ReadLine();
+                string line = Client.reader.ReadLine();
+                Log(line);
+                return line; 
             }
             catch
             {
-                Console.WriteLine("Unable to read from server");
+                Log("Unable to read from server", 0);
                 return null;
             }
         }
@@ -39,7 +97,7 @@ namespace Client
             try
             {
                 writer.WriteLine(Client.nick + " " + data);
-                Console.WriteLine("-->  " + data);
+                Log("-->  " + data, 3);
                 writer.Flush();
             }
             catch
@@ -52,7 +110,7 @@ namespace Client
             try
             {
                 Client.writer.WriteLine(Client.nick + " " + data);
-                Console.WriteLine("--> " + data);
+                Log("--> " + data, 3);
                 Client.writer.Flush();
             }
             catch
