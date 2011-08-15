45dec64e4f3760fb86f80fe787748d793fedb0ac
Icedream <icedream2k9@die-optimisten.net>
2011-08-15 17:37:33 +0200

    Fixed 1 issue and added batch files for "bin" and "obj" deletion & generation.
    - FIXED: Nickname configuration issue
    - ADDED: Batch file for compiling with Mono (using xbuild)
    - ADDED: Cleanup batch file for deleting "bin" and "obj" files (i think this is more useful to me only)

---
+++ Build-Mono.bat
@@ -0,0 +1,4 @@
+@echo off
+rem In case, some references fail (as in Microsoft VS)
+xbuild CSharpBot/CSharpBot.csproj
+xbuild CSharpBot.sln
---
+++ Cleanup.bat
@@ -0,0 +1,17 @@
+rm -r -v CSharpBot/bin
+rm -r -v CSharpBot/obj
+rm -r -v CSBConfig/bin
+rm -r -v CSBConfig/obj
+rm -r -v "CSharpBot Script Compiler/bin"
+rm -r -v "CSharpBot Script Compiler/obj"
+rm -r -v "SimpleLiveClient/bin"
+rm -r -v "SimpleLiveClient/obj"
+rm -r -v "WizardControls/bin"
+rm -r -v "WizardControls/obj"
+rm -r -v "CSharpBot Installer/CSharpBot Installer/Client"
+rm -r -v "CSharpBot Installer/CSharpBot Installer/Compiler"
+rm -r -v "CSharpBot Installer/CSharpBot Installer/CSharpBot"
+rm -r -v "CSharpBot Installer/CSharpBot Installer/Debug"
+rm -r -v "CSharpBot Installer/CSharpBot Installer/Installer"
+rm -r -v "CSharpBot Installer/CSharpBot Installer/LiveClient-Debug"
+rm -r -v "CSharpBot Installer/CSharpBot Installer/Release"
